1a87f5b577661db5886e703d3361160df0b8512d
Diego-FSP <diegomgplus014@hotmail.com>
2025-05-06 23:49:48 -0300

    fix bd
    are you will be ok

--- Comida DJZ/BD/INSERT 1.sql
+++ Comida DJZ/BD/INSERT 1.sql
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
+-- <<<<<<< HEAD
 -- Inserción de tipos de menú
 INSERT INTO TipoM (Nombre)
 VALUES 
 ('Comida'),   -- IDTipoM = 1
 ('Bebida'),   -- IDTipoM = 2
 ('Postre');   -- IDTipoM = 3
-=======
+-- =======
 -- Inserción de Menu
-INSERT INTO Menu (idMenu, Nombre, Cantidad, Precio, Descripcion)
-VALUES 
-(1,'Empanadas', 30, 900.00, 'Carne cortada'),
-(2,'Pollo Broster', 10, 1800.00, 'Crujiente y jugoso'),
-(3,'Hamburguesa', 15, 1200.00, 'Clásica con queso'),
-(4,'Salchipapa', 20, 1000.00, 'Papas con salchichas'),
-(5,'Puré c/Milanesa', 12, 1700.00, 'Con salsa o sin salsa');
->>>>>>> 3bb8df26bf0fc04954a09ccd048fb61bc9d4b1de
+
+-- >>>>>>> 3bb8df26bf0fc04954a09ccd048fb61bc9d4b1de
 
 -- Inserción de Menu (todos con tipo 'Comida' → IDTipo = 1)
 INSERT INTO Menu (Nombre, Cantidad, Precio, Descripcion, IDTipo)
@@ -35,3 +29,5 @@ SELECT idPedido FROM Pedido;  -- esto te muestra todos los IDs válidos
 INSERT INTO Ticked (idPedido, Nombre, mesa, hora)
 VALUES (1, 'Luis Fernández', FALSE, NOW());  -- solo si el 1 existe en Pedido
 
+select *
+from menu
